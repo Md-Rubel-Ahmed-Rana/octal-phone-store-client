059d13ff676e785ec917fad5a3b303b6768946bb
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-11-23 23:51:57 +0600

    Navbar completed

--- src/Shared/Navbar/Navbar.js
+++ src/Shared/Navbar/Navbar.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from "../../images/logo.png"
 
 const Navbar = () => {
     return (
-        <div className='flex justify-between px-20'>
-            <div>
-                <h2 className='text-2xl'>This is logo</h2>
+        <div className='flex justify-between items-center px-20 py-2 bg-blue-800 text-white'>
+            <div className='flex items-center gap-3'>
+                <img className='w-14 h-14 rounded-full' src={logo} alt="logo" />
+                <h2 className='lg:text-2xl font-bold'>Octal Phone Store</h2>
             </div>
             <div>
                 <ul className='flex gap-4'>
                     <li> <Link>Home</Link> </li>
-                    <li> <Link>Products</Link> </li>
+                    <li> <Link>My Products</Link> </li>
+                    <li> <Link>Add Product</Link> </li>
                     <li> <Link>Register</Link> </li>
                     <li> <Link>Login</Link> </li>
                 </ul>
