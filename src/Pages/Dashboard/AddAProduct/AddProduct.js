4e9d713b4ccab99a79b943940adb345ed86cc6fb
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-11-25 04:02:39 +0600

    404/not found page added

--- src/Pages/Dashboard/AddAProduct/AddProduct.js
+++ src/Pages/Dashboard/AddAProduct/AddProduct.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 const AddProduct = () => {
+    const { register, handleSubmit } = useForm();
+    const navigate = useNavigate()
+
+    const onSubmit = (data) => {
+        console.log(data);
+        navigate("/myproducts")
+    }
     return (
-        <div>
-            <h3 className="text-2xl text-black">This is add product</h3>
+        <div className="bg-black lg:py-10">
+            <div className="lg:w-1/2 bg-gray-800 mx-auto text-center lg:px-20 py-10 rounded-md">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="mb-3">
+                        <input className="p-2 w-2/3" type="email" {...register("email")} />
+                    </div>
+                    <div>
+                        <input className="p-2 w-2/3" type="password" {...register("password")} />
+                    </div>
+                    <div className="w-2/3 mx-auto mt-3">
+                        <input className="bg-blue-800 w-full px-5 py-2 rounded text-white cursor-pointer" type="submit" value="Add Product" />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
--- src/Shared/ErrorPage/ErrorPage.js
+++ src/Shared/ErrorPage/ErrorPage.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import notFound from "../../images/not-found.png"
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h3 className="text-2xl">This is Error page</h3>
+        <div className='bg-black h-screen py-10'>
+            <div className='w-96 mx-auto bg-gray-800  rounded-md p-10 text-center text-white'>
+                <img src={notFound} alt="" />
+                <p className='mt-3 leading-6'>Oops, The page you are looking is not found. Please try with correct route/path</p>
+                <Link to="/"><button className="bg-blue-700 px-10 mt-3 py-2 rounded">Go Back</button></Link>
+            </div>
         </div>
     );
 };
